2f6e17af47194f204adecf9a5cc26362f76618e4
Shrehan Raj Singh <shrehanofficial@gmail.com>
2022-01-24 09:07:46 +0530

    Linux additions

--- InstructionParser/ipsf_init.c
+++ InstructionParser/ipsf_init.c
@@ -4,6 +4,25 @@
 #include <Parser/dict_t.h>
 #include <builtins/built_in.h>
 
+#ifndef WIN32
+char *strrev(char *str)
+{
+    char *p1, *p2;
+
+    if (!str || !*str)
+        return str;
+    
+    for (p1 = str, p2 = str + strlen(str) - 1; p2 > p1; ++p1, --p2)
+    {
+        *p1 ^= *p2;
+        *p2 ^= *p1;
+        *p1 ^= *p2;
+    }
+    
+    return str;
+}
+#endif
+
 /**
  * MAIN:
  */
